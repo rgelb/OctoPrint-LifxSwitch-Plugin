441c44cc6c95cb223c3cba437082bedef4880899
rgelb <rgelb@vbrad.com>
2019-01-27 14:12:33 -0800

    updated url

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Robert Gelb"
 plugin_author_email = "rgelb@vbrad.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/rgelb/OctoPrint-Lifxswitch"
+plugin_url = "https://github.com/rgelb/OctoPrint-LifxSwitch-Plugin"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
